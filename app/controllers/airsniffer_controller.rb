b1bb0d9f9214d21e89d4fed890ef1020166e7a84
H2NCH2COOH <wzypublic@gmail.com>
2014-10-26 12:32:39 +0800

    Changed chart handler to read history points from file

--- app/controllers/airsniffer_controller.rb
+++ app/controllers/airsniffer_controller.rb
@@ -153,6 +153,7 @@ class AirsnifferController < ApplicationController
     end
     
     dev=Device.find_by dev_id: id, owner: uid
+    pdev=PreRegDevice.find_by dev_id: id
     
     if dev.nil?
       redirect_to '/404'
@@ -160,30 +161,38 @@ class AirsnifferController < ApplicationController
     end
     
     data=[]
-    endT=Time.now.utc
-    sixH=6*60*60
-    maxH=30*24
     
-    for i in 0..(maxH/6) do
-      url="http://api.xively.com/v2/feeds/#{dev.feed_id}/datastreams/PM25?duration=6hour&interval=0&end=#{endT.strftime '%FT%RZ'}"
-      url=URI.encode url
-      url=URI.parse url
-      req=Net::HTTP::Get.new url.to_s
-      req["X-ApiKey"]=dev.api_key
-      res=Net::HTTP.start(url.host, url.port){|http|http.request req}
-      j=JSON.parse res.body
-      
-      break unless j.has_key? 'datapoints'
-      
-      td=[]
+    File.open(Rails.root.join('device_history', dev.dev_id), 'r') do |f|
+      c=f.read
+    end
+    
+    c.insert 0, '['
+    if c.end_with? ','
+      c[-1]=']'
+    else
+      c<<']'
+    end
+    
+    j=JSON.parse c
+    j.each do |d|
+      data<<[d[0],d[1]]
+    end
+    
+    url="http://api.xively.com/v2/feeds/#{dev.feed_id}/datastreams/PM25?&interval=0&start=#{pdev.last_retrieve_time}"
+    url=URI.encode url
+    url=URI.parse url
+    req=Net::HTTP::Get.new url.to_s
+    req["X-ApiKey"]=dev.api_key
+    res=Net::HTTP.start(url.host, url.port){|http|http.request req}
+    j=JSON.parse res.body
+    
+    if j.has_key? 'datapoints'
       j['datapoints'].each do |d|
         v=d['value']
         t=d['at']
         x=DateTime.strptime t, '%FT%T.%LZ'
-        td<<[x.to_time.to_i*1000, v.to_i]
+        data<<[x.to_time.to_i*1000, v.to_i]
       end
-      data=td.concat data
-      endT-=sixH
     end
     
     @dataCount=data.size
